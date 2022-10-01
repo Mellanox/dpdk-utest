ba9096393d039315ad319053822580d413c9e53d
Gregory Etelson <getelson@nvidia.com>
2022-10-01 18:33:14 +0300

    DPDK utest version 2.0

--- README.md
+++ README.md
@@ -1,1 +1,81 @@
-User guide: https://nvidia-my.sharepoint.com/:p:/p/getelson/EREjLu0r_mlIkhRRTWTyegoBHRBSE3zPG_g08vXS5UUUmw?e=gTUGMK
+# dpdk-utest
+
+## configuration file format:
+```yaml
+dut:  
+  # DUT hostname or IP address  
+  host:  
+  # DUT username  
+  username:  
+  # DUT user password  
+  password:  
+  # DUT path to test program  
+  path:  
+  # DUT ports for EAL commandline  
+  ports: [ PCI-port0, ... ]  
+  
+tg:  
+  # TG hostname or IP address  
+  host:  
+  # TG username  
+  username:  
+  # TG password  
+  password:  
+  # network interface to send packets to DUT  
+  if0:  
+  # network interface to receive packets from DUT  
+  if1:  
+```
+
+## test file format
+``` yaml
+prog: 'dpdk-testpmd -a PORT_0 -a PORT_1 -- -i'
+
+flow:
+  -
+    phases: [ *phase1 ]
+    repeat: 1
+  -
+    phases: [ *phase2, *phase3 ]
+    repeat: 3
+    
+p: &phase
+  # phase name - mandatory
+  name:
+  
+  # PMD commands - optional
+  pmd:
+    - 
+      # testpmd commands
+      command:
+      # string to match after command execution or None
+      result:
+
+  # TG command - optional     
+  tg:
+
+  # phase results to match - optional
+  results:
+    pmd:
+    tg:
+    vm:
+      
+  # phase repeat counter - optional
+  repeat:
+```      
+  
+### Phase execution order:
+Execution order of `pmd` and `tg` keys depends on the keys position.
+```
+phase1:
+    name:
+    pmd:
+    tg:
+
+phase2:
+    name:
+    tg:
+    pmd:
+```
+
+In `phase1` `tg` follows `pmd` while in `phase2` `pmd` follows `tg`.    
---
+++ utest.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python3
+
+###
+# ./utest.py  --config config.yaml --test test.yaml
+###
+
+from conf import TestConf
+from rcmd import *
+
+class Agents:
+    testpmd = None
+    tg = None
+    vm = None
+
+    def __init__(self, conf:dict):
+        self.testpmd = TestPMD(conf)
+        if 'tg' in conf.data:
+            self.tg = Scapy(conf.data['tg'], 'TG')
+        if 'vm' in conf.data:
+            self.vm = Scapy(conf.data['vm'], 'VM')
+
+    def close(self):
+        for obj in [ self.testpmd, self.tg, self.vm ]:
+            if obj is not None: obj.close()
+
+
+def do_phase(agents:Agents, phase:dict):
+    repeat = phase['repeat'] if 'repeat' in phase else 1
+    for i in range(0, repeat):
+        logging.info("#### PHASE: " + phase['name'] + ' ######')
+        for key in phase.keys():
+            # first, run commands in order specified by the phase
+            if key == 'pmd': agents.testpmd.execute(phase['pmd'])
+            if key == 'tg': agents.tg.execute(phase['tg'])
+            if key == 'vm': agents.vm.execute(phase['vm'])
+            if key == 'result':
+                result = phase['result']
+                if 'pmd' in result: agents.testpmd.match(result['pmd'])
+                if 'tg' in result: agents.tg.match(result['tg'])
+                if 'vm' in result: agents.vm.match(result['vm'])
+
+
+if __name__ == "__main__":
+    conf = TestConf()
+    if conf.args.test is None: exit(0)
+
+    try:
+        agents = Agents(conf)
+
+        for flow in conf.test['flow']:
+            for i in range(0, flow['repeat']):
+                for phase in flow['phases']: do_phase(agents, phase)
+
+        logging.info('=== TEST COMPLETED')
+        # must be called explicitly to prevent paramiko crash - known issue
+        agents.close()
+
+    except Exception as e:
+        logging.error("Test failed: ", e.__class__)
+
